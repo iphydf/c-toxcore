a03a87d3de81b39fbd9eacb024934570e109ec14
ingvar1995 <igorn1995@yandex.ru>
2018-02-20 23:22:01 +0300

    join public gc via invite - ann sending fix

--- toxcore/group_chats.c
+++ toxcore/group_chats.c
@@ -2014,17 +2014,11 @@ static int broadcast_gc_shared_state(GC_Chat *chat)
  */
 static void do_gc_shared_state_changes(GC_Session *c, const GC_Chat *chat, const GC_SharedState *old_shared_state)
 {
-    if (old_shared_state->version == 0) {
-        return;
-    }
-
     /* Max peers changed */
     if (chat->shared_state.maxpeers != old_shared_state->maxpeers) {
         if (c->peer_limit) {
             (*c->peer_limit)(c->messenger, chat->groupnumber, chat->shared_state.maxpeers, c->peer_limit_userdata);
         }
-
-        return;
     }
 
     /* privacy state changed */
@@ -2040,8 +2034,6 @@ static void do_gc_shared_state_changes(GC_Session *c, const GC_Chat *chat, const
             m_remove_friend_gc(c->messenger, chat);
             cleanup_gca(c->announces_list, CHAT_ID(chat->chat_public_key));
         }
-
-        return;
     }
 
     /* password changed */
@@ -2052,8 +2044,6 @@ static void do_gc_shared_state_changes(GC_Session *c, const GC_Chat *chat, const
             (*c->password)(c->messenger, chat->groupnumber, chat->shared_state.passwd,
                            chat->shared_state.passwd_len, c->password_userdata);
         }
-
-        return;
     }
 }
 
@@ -6109,6 +6099,7 @@ int gc_accept_invite(GC_Session *c, int32_t friend_number, const uint8_t *data, 
     expand_chat_id(chat->chat_public_key, chat_id);
     chat->chat_id_hash = get_chat_id_hash(CHAT_ID(chat->chat_public_key));
     chat->join_type = HJ_PRIVATE;
+    chat->shared_state.privacy_state = GI_PRIVATE;
     chat->last_join_attempt = unix_time();
 
     if (passwd != nullptr && passwd_len > 0) {
