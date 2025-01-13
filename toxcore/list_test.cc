6086a9d3a4af0226e0eed4dfa80803005e2a1653
iphydf <iphydf@users.noreply.github.com>
2025-01-13 16:18:12 +0000

    cleanup: Use tox memory allocator in some more places.

--- toxcore/list_test.cc
+++ toxcore/list_test.cc
@@ -2,26 +2,31 @@
 
 #include <gtest/gtest.h>
 
+#include "mem_test_util.hh"
+
 namespace {
 
 TEST(List, CreateAndDestroyWithNonZeroSize)
 {
+    Test_Memory mem;
     BS_List list;
-    bs_list_init(&list, sizeof(int), 10, memcmp);
+    bs_list_init(&list, mem, sizeof(int), 10, memcmp);
     bs_list_free(&list);
 }
 
 TEST(List, CreateAndDestroyWithZeroSize)
 {
+    Test_Memory mem;
     BS_List list;
-    bs_list_init(&list, sizeof(int), 0, memcmp);
+    bs_list_init(&list, mem, sizeof(int), 0, memcmp);
     bs_list_free(&list);
 }
 
 TEST(List, DeleteFromEmptyList)
 {
+    Test_Memory mem;
     BS_List list;
-    bs_list_init(&list, sizeof(int), 0, memcmp);
+    bs_list_init(&list, mem, sizeof(int), 0, memcmp);
     const uint8_t data[sizeof(int)] = {0};
     bs_list_remove(&list, data, 0);
     bs_list_free(&list);
